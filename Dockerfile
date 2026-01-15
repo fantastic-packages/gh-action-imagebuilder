9a9f1cc3ab2ab2c1d9fd2eb3c74bdb00acca545c
Anya Lin <hukk1996@gmail.com>
2026-01-15 16:33:27 +0800

    improvements

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-ARG CONTAINER=ghcr.io/openwrt/imagebuilder
-ARG ARCH=ath79-nand
-FROM $CONTAINER:$ARCH
-
-LABEL "com.github.actions.name"="OpenWrt ImageBuilder"
-
-ADD entrypoint.sh /
-
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,24 @@ endgroup() {
 }
 trap 'endgroup' ERR
 
+group "download setup.sh"
+wget -O setup.tar.gz https://codeload.github.com/openwrt/docker/tar.gz/refs/heads/main
+tar xf setup.tar.gz --strip=1 --no-same-owner -C .
+rm -vrf setup.tar.gz
+
+sed -i 's|/builder/keys/|keys/|g' setup.sh
+[ "$CACHE" = "true" ] && sed -i '/wget .*\$file_name/d' setup.sh \
+|| sed -i '/wget .*\$file_name/{s|wget -nv|axel -q -H "'"User-Agent: $USER_AGENT"'" -n8|g}' setup.sh
+
+echo -e "\nsetup.sh START"
+cat setup.sh
+echo -e "setup.sh END\n"
+endgroup
+
 group "bash setup.sh"
 # snapshot containers don't ship with the ImageBuilder to save bandwidth
 # run setup.sh to download and extract the ImageBuilder
-[ ! -f setup.sh ] || bash setup.sh
+bash setup.sh
 endgroup
 
 # rules
@@ -30,7 +44,7 @@ export TOPDIR=$(pwd)
 export OUTPUT_DIR=$TOPDIR/bin
 export BIN_DIR=$OUTPUT_DIR/targets/$BOARD/$SUBTARGET
 export SCRIPT_DIR=$TOPDIR/scripts
-export OPKG_KEYS=$TOPDIR/keys
+export KEYS_DIR=$TOPDIR/keys
 export BUILD_KEY=$TOPDIR/key-build
 export BUILD_KEY_APK_SEC=$TOPDIR/keys/local-private-key.pem
 export BUILD_KEY_APK_PUB=$TOPDIR/keys/local-public-key.pem
@@ -38,83 +52,49 @@ export STAGING_DIR_HOST=$TOPDIR/staging_dir/host
 PATHBK="$PATH"
 export PATH="$STAGING_DIR_HOST/bin:$PATH"
 
+# Initialize bin/ symlink
 for d in bin; do
-	mkdir -p /artifacts/$d 2>/dev/null
-	ln -s /artifacts/$d $d
+	mkdir -p $artifacts_dir/$d 2>/dev/null
+	ln -s $artifacts_dir/$d $d
 done
 
+# opkg key-build
 if [ -n "$KEY_BUILD" ]; then
 	echo "$KEY_BUILD" > $BUILD_KEY
-	SIGN="1"
-fi
-if [ -n "$KEY_BUILD_PUB" ]; then
-	echo "$KEY_BUILD_PUB" > $BUILD_KEY.pub
-	$SCRIPT_DIR/opkg-key add $BUILD_KEY.pub
-	ADD_LOCAL_KEY="1"
-fi
-if [ -n "$KEY_VERIFY" ]; then
-	for _key in $KEY_VERIFY; do
-		base64 -d <<< "$_key" > /tmp/_key
-		$SCRIPT_DIR/opkg-key add /tmp/_key
-	done
+	SIGN_IMG="1"
 fi
 
+# apk private-key.pem
 if [ -n "$PRIVATE_KEY" ]; then
 	echo "$PRIVATE_KEY" > $BUILD_KEY_APK_SEC
 	openssl ec -in $BUILD_KEY_APK_SEC -pubout > $BUILD_KEY_APK_PUB
 	ADD_LOCAL_KEY="1"
-	SIGN="1"
 fi
 if [ -n "$PUBLIC_KEY_VERIFY" ]; then
 	for _key in $PUBLIC_KEY_VERIFY; do
 		base64 -d <<< "$_key" > /tmp/_key
-		cp -f /tmp/_key $OPKG_KEYS/$(md5sum /tmp/_key | awk '{print $1}').pem
+		cp -f /tmp/_key $KEYS_DIR/$(md5sum /tmp/_key | awk '{print $1}').pem
 	done
 fi
 
-group "ls -R $OPKG_KEYS"
-ls -R $OPKG_KEYS
+group "ls -R $KEYS_DIR"
+ls -R $KEYS_DIR
 endgroup
 
-if [ -n "$USE_APK" ]; then
-
 n=$(sed -n '$=' repositories)
-
 if [ -n "$NO_DEFAULT_REPOS" ]; then
-	sed -i 's|^http|# http|g' repositories
+	sed -i 's|^http|# http|' repositories
 fi
 if [ -z "$NO_LOCAL_REPOS" ]; then
-	sed -i "${n}a\\file:///repo/packages.adb" repositories
+	sed -i "${n}a\\file://$REPO_DIR/packages.adb" repositories
 fi
 for EXTRA_REPO in $EXTRA_REPOS; do
 	sed -i "${n}a\\$EXTRA_REPO" repositories
 done
+
 group "repositories"
 cat repositories
 endgroup
-
-else # use opkg
-
-regexp='src imagebuilder file:packages'
-
-if [ -n "$NO_DEFAULT_REPOS" ]; then
-	sed -i 's|^src/gz|## src/gz|g' repositories.conf
-fi
-if [ -z "$NO_LOCAL_REPOS" ]; then
-	sed -i "/$regexp/i\\src custom file:///repo/" repositories.conf
-fi
-for EXTRA_REPO in $EXTRA_REPOS; do
-	sed -i "/$regexp/i\\$(tr '|' ' ' <<< "$EXTRA_REPO")" repositories.conf
-done
-if [ -n "$NO_SIGNATURE_CHECK" ]; then
-	sed -i 's|^option check_signature|## option check_signature|' repositories.conf
-fi
-
-group "repositories.conf"
-cat repositories.conf
-endgroup
-
-fi
 
 if [ -n "$ROOTFS_SIZE" ]; then
 	sed -i "s|\(\bCONFIG_TARGET_ROOTFS_PARTSIZE\)=.*|\1=$ROOTFS_SIZE|" .config
@@ -123,17 +103,18 @@ fi
 RET=0
 
 export PATH="$PATHBK"
+
+group "make image"
 make image \
 	PROFILE="$PROFILE" \
 	DISABLED_SERVICES="$DISABLED_SERVICES" \
 	ADD_LOCAL_KEY="$ADD_LOCAL_KEY" \
 	PACKAGES="$PACKAGES" || RET=$?
+endgroup
 
-if [ "$SIGN" = '1' ];then
+if [ "$SIGN_IMG" = '1' -a -n "$KEY_BUILD" ];then
 	pushd $BIN_DIR
-	if [ -z "$USE_APK" ]; then
 	$STAGING_DIR_HOST/bin/usign -S -m sha256sums -s $BUILD_KEY
-	fi
 	popd
 fi
 
